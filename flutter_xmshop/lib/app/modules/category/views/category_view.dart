894163cd4b3e4430425eed3933962138cd19230f
shaozejun <ios_developjason@163.com>
2022-12-09 14:48:22 +0800

    feat: 分类页面的导航条搭建

--- flutter_xmshop/lib/app/modules/category/views/category_view.dart
+++ flutter_xmshop/lib/app/modules/category/views/category_view.dart
@@ -1,19 +1,59 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_xmshop/app/tool/screenadapter.dart';
 import 'package:get/get.dart';
-
 import '../controllers/category_controller.dart';
 
 class CategoryView extends GetView<CategoryController> {
   const CategoryView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'CategoryView is working',
-          style: TextStyle(fontSize: 20),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        title: Padding(
+          padding: EdgeInsets.fromLTRB(ScreenAdpater.width(10),
+              ScreenAdpater.height(20), ScreenAdpater.width(60), 0),
+          child: Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+            width: double.infinity,
+            height: ScreenAdpater.height(88),
+            decoration: BoxDecoration(
+                color: const Color.fromRGBO(246, 247, 248, 1),
+                borderRadius: BorderRadius.circular(20)),
+            child: Row(
+              children: [
+                Image.asset("assets/images/xmsearch.png"),
+                const Text(
+                  "请输入商品名称...",
+                  style: TextStyle(
+                      fontSize: 14, color: Color.fromRGBO(156, 157, 158, 1)),
+                )
+              ],
+            ),
+          ),
         ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Image.asset("assets/images/xmmessage.png"),
+          ),
+          SizedBox(
+            width: ScreenAdpater.width(10),
+          ),
+        ],
+      ),
+      body: _setupBody(),
+    );
+  }
+
+  Widget _setupBody() {
+    return Center(
+      child: Text(
+        'CategoryView is working',
+        style: TextStyle(fontSize: 20),
       ),
     );
   }
